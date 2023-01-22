98de504af1793db07fc4ef73d567869bd5787c9f
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-22 13:09:34 +0500

    style(config): changed line breaks and indents

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,13 @@ const {Sequelize} = require('sequelize');
 
 const config = require('./index')
 
-const sequelize = new Sequelize(config.database.name, config.database.username, config.database.password, {
-    dialect: 'postgres',
-    host: config.database.host,
-    port: config.database.port
-});
-
-module.exports = sequelize;
+module.exports = new Sequelize(
+    config.database.name,
+    config.database.username,
+    config.database.password,
+    {
+        dialect: 'postgres',
+        host: config.database.host,
+        port: config.database.port
+    }
+);
