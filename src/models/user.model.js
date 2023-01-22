3812ad5cf8cc7f2e7d681f575dcd52dcfdff958b
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-22 13:40:23 +0500

    feat(db): added `User` model

---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+const {DataTypes} = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('User', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        email: {
+            type: DataTypes.STRING,
+            validate: { isEmail: true },
+        },
+        password: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        bio: {
+            type: DataTypes.TEXT
+        },
+    }, {
+        timestamps: true,
+        createdAt: true,
+        updatedAt: false
+    });
+};
