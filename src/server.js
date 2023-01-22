d77cbcdb78a5bf4cd84927f7da42c36d58ba1f41
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-22 15:26:36 +0500

    feat(server): added database initialization

--- src/server.js
+++ src/server.js
@@ -1,6 +1,28 @@
 const app = require('./app')
 const config = require('./config')
+const sequelize = require('./config/db.config')
+const models = require('./models')
 
-app.listen(config.port, () => {
-    console.log(`Application is running on port ${config.port}`)
-})
+console.log(config);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize
+            .sync({alter: true})
+            .then(() => {
+                console.log('Connection with database established successfully');
+            })
+            .catch(() => {
+                console.log('Some errors have occurred during establishing connection with database');
+            })
+        app.listen(config.port, () => {
+            console.log(`Application is running on port ${config.port}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+start()
