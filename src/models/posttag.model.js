b63d8a303ce0a9ad8b8dc7f635054294a89f1aac
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-22 15:17:02 +0500

    feat(db): added `PostTag` model

---
+++ src/models/posttag.model.js
@@ -0,0 +1,31 @@
+const {DataTypes} = require('sequelize');
+const {PostModel} = require('./post.model')
+const {TagModel} = require('./tag.model')
+const sequelize = require('../config/db.config')
+
+module.exports = () => {
+    sequelize.define('PostTag', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        PostId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: PostModel,
+                key: 'id'
+            }
+        },
+        TagId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: TagModel,
+                key: 'id'
+            }
+        },
+    }, {
+        timestamps: false,
+    });
+};
