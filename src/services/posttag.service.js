7ba84273174053b30c4f46b20fb5092f0540af5f
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-27 22:07:56 +0500

    feat(posttag.service): added `PostTagService`

---
+++ src/services/posttag.service.js
@@ -0,0 +1,22 @@
+const {PostTagModel} = require('../models')
+
+class PostTagService {
+    async bulkCreate(tags) {
+        await PostTagModel.bulkCreate(tags)
+            .catch((error) => {
+                console.log(error);
+                throw new Error('Something went wrong');
+            });
+    }
+
+    async delete(postId) {
+        await PostTagModel
+            .destroy({where: {postId: postId}})
+            .then(() => ({status: true, message: 'PostTag Removed'}))
+            .catch((error) => {
+                throw new Error(`PostTag Delete Operation Failed: ${error.message}`);
+            });
+    }
+}
+
+module.exports = new PostTagService()
