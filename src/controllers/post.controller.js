52d5e1267538f5ba38686516f0fc82de6dc5f449
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-28 16:30:18 +0500

    feat(post.controller): added create function

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,10 +1,44 @@
 const asyncHandler = require('express-async-handler')
 const createError = require('http-errors')
-const TagService = require('../services/post.service')
+const TagService = require('../services/tag.service')
+const PostService = require('../services/post.service')
+const PostTagService = require('../services/posttag.service')
 
 
 class PostController {
+    create = asyncHandler(async (req, res, next) => {
+        const {banner, title, content, userId, tags} = req.body
 
+        if (tags.length > 5) {
+            throw createError(400, 'The maximum number of tags is 5')
+        }
+
+        try {
+            const post = await PostService.create({
+                banner: banner,
+                title: title,
+                content: content,
+                userId: userId
+            })
+
+            let postTagRelationships = []
+            for (const tag of tags) {
+                const tag = await TagService.retrieveOne(tag)
+                if (tag) {
+                    postTagRelationships.push({
+                        PostId: post.dataValues.id,
+                        TagId: tag.dataValues.id
+                    })
+                }
+            }
+            await PostTagService.bulkCreate(postTagRelationships);
+
+            res.status(200).json(post)
+
+        } catch (e) {
+            throw createError(400, e.message)
+        }
+    })
 }
 
 module.exports = new PostController()
