b881f87dc93f9de40cbf66da520a69fadc5efa31
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-02 19:17:24 +0500

    feat(core): restructured base of application

--- src/app.js
+++ src/app.js
@@ -6,33 +6,59 @@ const xss = require('xss-clean')
 const fileUpload = require('express-fileupload')
 
 const config = require('./config')
-const routes = require('./api')
+const apiRoutes = require('./api')
+const database = require("./config/db.config");
 
-const app = express()
 
-// Allow Cross-Origin requests
-app.use(cors());
-// Set security HTTP headers
-app.use(helmet());
-// Data sanitization against XSS(clean user input from malicious HTML code)
-app.use(xss());
-// Transforms the raw string of req.body into json
-app.use(express.json());
-// Static content
-app.use(express.static(path.resolve(__dirname, '..', 'static')))
-// Allow receiving files
-app.use(fileUpload({}))
-// Load API
-app.use(config.api.prefix, routes);
-// Errors handler
-app.use((error, req, res, next) => {
-    // Sets HTTP status code
-    res.status(error.status || 500)
-    // Sends response
-    res.json({
-        status: error.status || 500,
-        message: error.message,
-    })
-})
+class Application {
+    constructor() {
+        this.app = express()
+        this.config = config
+        this.db = database
+    }
 
-module.exports = app
+    setupDatabase = async () => {
+        await this.db.authenticate()
+        await this.db
+            .sync({alter: true})
+            .then(() => {
+                console.log('Connection with database established successfully');
+            })
+            .catch(() => {
+                console.log('Some errors have occurred during establishing connection with database');
+            })
+    }
+
+    setupExpress = async () => {
+        // Allow Cross-Origin requests
+        this.app.use(cors());
+        // Set security HTTP headers
+        this.app.use(helmet());
+        // Data sanitization against XSS(clean user input from malicious HTML code)
+        this.app.use(xss());
+        // Transforms the raw string of req.body into json
+        this.app.use(express.json());
+        // Static content
+        this.app.use(express.static(path.resolve(__dirname, '..', 'static')))
+        // Allow receiving files
+        this.app.use(fileUpload({}))
+        // Load API
+        this.app.use(this.config.api.prefix, apiRoutes);
+        // Errors handler
+        this.app.use((error, req, res, next) => {
+            res.status(error.status || 500)
+            res.json({
+                status: error.status || 500,
+                message: error.message,
+            })
+        })
+    }
+
+    setupServer = async () => {
+        this.app.listen(this.config.port, () => {
+            console.log(`Application is running on port ${this.config.port}`)
+        })
+    }
+}
+
+module.exports = new Application()
--- src/server.js
+++ src/server.js
@@ -1,25 +1,9 @@
-const app = require('./app')
-const config = require('./config')
-const sequelize = require('./config/db.config')
+const Application = require('./app')
 
-const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize
-            .sync({alter: true})
-            .then(() => {
-                console.log('Connection with database established successfully');
-            })
-            .catch(() => {
-                console.log('Some errors have occurred during establishing connection with database');
-            })
-        app.listen(config.port, () => {
-            console.log(`Application is running on port ${config.port}`)
-        })
-    } catch (e) {
-        console.log(e)
-    }
+async function main() {
+    await Application.setupDatabase()
+    await Application.setupExpress()
+    await Application.setupServer()
 }
 
-
-start()
+main()
