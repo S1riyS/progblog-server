d3e657da90556b195fa1fdcbb195e12683fc6caf
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-05 21:58:48 +0500

    feat(auth.middleware): added `checkToken` function

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,17 +2,21 @@ const jwt = require('jsonwebtoken')
 const ApiError = require('../errors/apiError')
 const config = require('../config')
 
+function checkToken(token) {
+    if (!token) {
+        throw ApiError.authRequired('Not authorized')
+    }
+}
+
 module.exports = function (req, res, next) {
     if (req.method === "OPTIONS") {
         next()
     }
 
-    const token = req.headers.authorization.split(' ')[1] // Bearer *token*
-    if (!token) {
-        throw ApiError.authRequired('Not authorized')
-    }
-
     try {
+        const token = req.headers.authorization.split(' ')[1] // Bearer *token*
+        checkToken(token)
+
         req.user = jwt.verify(token, config.jwt.secret_key)
         next()
     } catch (e) {
