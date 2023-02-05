c229d020eb8ce0d1bb17f13c8a9e4d2461985bbc
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-05 19:07:56 +0500

    feat(comment.router): data validation during creation

--- src/api/comment.router.js
+++ src/api/comment.router.js
@@ -1,9 +1,14 @@
 const Router = require("express");
 const router = new Router()
+const {check} = require("express-validator")
+const validate = require('../middlewares/validation.middleware')
 const authMiddleware = require('../middlewares/auth.middleware')
 const CommentController = require('../controllers/comment.controller')
 
-router.post('/', authMiddleware, CommentController.create)
+router.post('/', [
+    check('text', 'Comment can not be empty').notEmpty()
+], authMiddleware, validate, CommentController.create)
+
 router.get('/:id', CommentController.getOne)
 router.get('/post/:postId', CommentController.getByPost)
 router.get('/user/:userId', CommentController.getByUser)
