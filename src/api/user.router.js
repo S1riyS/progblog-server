c6286954f64ae319528b1a6aa4374bc0b063c52d
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-05 18:57:45 +0500

    feat(user.router): added data validation for registration

--- src/api/user.router.js
+++ src/api/user.router.js
@@ -1,9 +1,16 @@
 const Router = require("express");
 const router = new Router()
+const {check} = require("express-validator")
 const authMiddleware = require('../middlewares/auth.middleware')
+const validate = require('../middlewares/validation.middleware')
 const UserController = require('../controllers/user.controller')
 
-router.post('/register', UserController.register)
+router.post('/register', [
+    check('email', 'Incorrect email').isEmail(),
+    check('password', 'Minimal length of password is 4').isLength({min: 4}),
+    check('name', 'Name can not be empty').notEmpty(),
+], validate, UserController.register)
+
 router.post('/login', UserController.login)
 router.get('/auth', authMiddleware, UserController.auth)
 router.get('/', UserController.getAll)
