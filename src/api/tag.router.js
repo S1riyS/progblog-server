23fb910c80e47de0adfa7d502cf3c33c17944fe4
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-05 19:10:02 +0500

    feat(tag.router): data validation during creation

--- src/api/tag.router.js
+++ src/api/tag.router.js
@@ -1,8 +1,14 @@
 const Router = require("express");
 const router = new Router()
+const {check} = require("express-validator")
+const validate = require('../middlewares/validation.middleware')
 const TagController = require('../controllers/tag.controller')
 
-router.post('/', TagController.create)
+router.post('/', [
+    check('name', 'Tag name can not be empty').notEmpty(),
+    check('description', 'Tag description can not be empty').notEmpty()
+], validate, TagController.create)
+
 router.get('/', TagController.getAll)
 router.get('/:tagName', TagController.getOne)
 
