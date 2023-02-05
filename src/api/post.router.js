6a497fea3fd7888cce3a0cc4e5fac58d4e0c7adc
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-05 19:05:58 +0500

    feat(post.router): data validation during creation

--- src/api/post.router.js
+++ src/api/post.router.js
@@ -1,8 +1,14 @@
 const Router = require("express");
 const router = new Router()
+const {check} = require("express-validator")
+const validate = require('../middlewares/validation.middleware')
 const PostController = require('../controllers/post.controller')
 
-router.post('/', PostController.create)
+router.post('/', [
+    check('title', 'Title can not be empty').notEmpty(),
+    check('content', 'Content can not be empty').notEmpty(),
+], validate, PostController.create)
+
 router.get('/', PostController.getAll)
 router.get('/tag/:tagName', PostController.getByTag)
 router.get('/:id', PostController.getOne)
