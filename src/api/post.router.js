9472cd0b056b8519e5e7236179b22c9c225774a1
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-02-05 22:41:25 +0500

    feat(post.router): changed order of middlewares

--- src/api/post.router.js
+++ src/api/post.router.js
@@ -5,10 +5,10 @@ const validate = require('../middlewares/validation.middleware')
 const authMiddleware = require('../middlewares/auth.middleware')
 const PostController = require('../controllers/post.controller')
 
-router.post('/', [
+router.post('/', authMiddleware, [
     check('title', 'Title can not be empty').notEmpty(),
     check('content', 'Content can not be empty').notEmpty(),
-], validate, authMiddleware, PostController.create)
+], validate, PostController.create)
 
 router.get('/', PostController.getAll)
 router.get('/tag/:tagName', PostController.getByTag)
